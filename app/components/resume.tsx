683d119d51d226d42493bc9ad81b1f56c7b7f170
Jaden Wong <jadenw2542@gmail.com>
2025-01-18 00:23:01 -0500

    resume

--- app/components/resume.tsx
+++ app/components/resume.tsx
@@ -1,5 +1,5 @@
 export default function Resume() {
-  const resumePDF = '/Jaden_Wong_Resume_2024.pdf';
+  const resumePDF = '/Jaden_Wong_Resume_2025.pdf';
 
   return (
     <div className="flex flex-grow pb-5 mobile:h-96 mobile:w-full mobile:flex-none">
